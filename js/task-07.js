592b7b4402636a9dc63627c6eb44eba8c2fa50a6
Carlos61Gomez <127167485+Carlos61Gomez@users.noreply.github.com>
2024-01-23 20:40:04 -0500

    home

--- js/task-07.js
+++ js/task-07.js
@@ -1,25 +1,7 @@
+const fontSizeControl = document.getElementById("font-size-control");
+const textElement = document.getElementById("text");
 
-const validationInput = document.getElementById('validation-input');
-
-validationInput.addEventListener('blur', function() {
-  const inputValue = validationInput.value;
-  const requiredSymbols = parseInt(validationInput.dataset.length);
-
-  if (inputValue.length === requiredSymbols) {
-    validationInput.classList.remove('invalid');
-    validationInput.classList.add('valid');
-  } else {
-    validationInput.classList.remove('valid');
-    validationInput.classList.add('invalid');
-  }
-});
-
-
-
-const fontSizeControl = document.getElementById('font-size-control');
-const text = document.getElementById('text');
-
-fontSizeControl.addEventListener('input', function() {
-  const fontSize = fontSizeControl.value + 'px';
-  text.style.fontSize = fontSize;
+fontSizeControl.addEventListener("input", function () {
+  const fontSize = fontSizeControl.value + "px";
+  textElement.style.fontSize = fontSize;
 });
